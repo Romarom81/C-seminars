5ec6d96bba1d99d1a85ccd34a5221c1063767b36
Roman Romanenko <romarom81@mail.ru>
2022-06-28 18:11:29 +0300

    дописал решение для Task004

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -3,6 +3,10 @@ Console.WriteLine ("Введите любое целое число:");
 int number = Convert.ToInt32( Console.ReadLine ());
 
 OurPower (number); //вызов метода
+OurPower (number+number);
+OurPower (36);
+OurPower (number+number+number);
+OurPower (124);
 
 void OurPower (int currentNumber) 
 {
