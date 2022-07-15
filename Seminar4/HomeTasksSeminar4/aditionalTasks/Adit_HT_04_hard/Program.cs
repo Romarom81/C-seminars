0711ec1126911a030c4a3a878247e828359f7647
Roman Romanenko <romarom81@mail.ru>
2022-07-15 12:13:04 +0300

    редактировал структуру

--- Seminar4/HomeTasksSeminar4/aditionalTasks/Adit_HT_04_hard/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Задача 1*. С помощью команд очистки консоли и печати реализовать игру-бродилку в консоли.
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.clear?view=net-6.0
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.setcursorposition?view=net-6.0
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.readkey?view=net-6.0
- // https://docs.microsoft.com/ru-ru/dotnet/api/system.consolekey?view=net-6.0
-
